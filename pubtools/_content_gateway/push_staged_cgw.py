89e66e460a67eda4ecbdaad812fbb61bc2cd3190
Javed Alam <jalam@redhat.com>
2022-07-22 17:16:38 +0530

    Updating Doc strings     - Added pub support for Content Gateway

--- pubtools/_content_gateway/push_staged_cgw.py
+++ pubtools/_content_gateway/push_staged_cgw.py
@@ -38,7 +38,7 @@ class PushStagedCGW(PushBase):
         Invoked during task execution after successful completion of all Pulp
         publishes.
 
-        This hook is invoked a maximum of once per task, to indicate that all Pulp
+        This hook is invoked for every push item, to indicate that all Pulp
         content associated with the task is considered fully up-to-date. The
         intended usage is to flush Pulp-derived caches or to notify systems that
         Pulp content may have recently changed.
