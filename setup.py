83db1576d9e3316f86bb574553f2b26d391ad64c
Jindrich Luza <jluza@redhat.com>
2023-01-31 15:46:06 +0100

    Release 0.3.1

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ classifiers = [
 
 setup(
     name="pubtools-content-gateway",
-    version="0.3.0",
+    version="0.3.1",
     description=get_description(),
     long_description_content_type="text/markdown",
     author="Javed Alam",
